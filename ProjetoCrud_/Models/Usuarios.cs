5c1ef94dce43da9e931a2a23dc35ab65fbe62c7a
Dario Gabriel <105221369+DariWesT@users.noreply.github.com>
2023-01-02 21:12:59 +0000

    Add

---
+++ ProjetoCrud_/Models/Usuarios.cs
@@ -0,0 +1,45 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ProjetoCrud_.Models
+{
+    [Table("Usuario")]
+    public class Usuarios
+    {
+        [Display(Name = "Código")]
+        [Column("Id")]
+        public int Id { get; set; }
+
+        [Display(Name = "Nome")]
+        [Column("Nome")]
+        public string Nome { get; set; }
+
+        [Display(Name = "Rua")]
+        [Column("Rua")]
+        public string Rua { get; set; }
+
+        [Display(Name = "Número")]
+        [Column("Numero")]
+        public int Numero { get; set; }
+
+        [Display(Name = "Complemento")]
+        [Column("Complemento")]
+        public string Complemento { get; set; }
+
+        [Display(Name = "Localidade")]
+        [Column("Bairro")]
+        public string Bairro { get; set; }
+
+        [Display(Name = "CódigoPostal")]
+        [Column("Cep")]
+        public int Cep { get; set; }
+
+        [Display(Name = "Cidade")]
+        [Column("Cidade")]
+        public string Cidade { get; set; }
+
+        [Display(Name = "Distrito")]
+        [Column("Estado")]
+        public string Estado { get; set; }
+    }
+}
